8bfb407cf1f5c4475a27d276cb6d2870f0f6615d
Hargun Mujral <hargunmujral@gmail.com>
2021-09-18 19:31:31 -0400

    additional code

--- Hargun/imagetext.js
+++ Hargun/imagetext.js
@@ -1,4 +1,5 @@
 const tesseract = require("tesseract.js");
+// const fs = require("fs")
 
 tesseract
     .recognize("test.jpg", "eng", {logger: (m) => console.log(m),
@@ -9,3 +10,33 @@ tesseract
     .catch((err) => {
         console.log(err.message);
     });
+
+// const getImageText = async (fileName, lang, logger) => {
+//     let recognizeResult = null
+//     try {
+//         if (fs.existsSync(fileName)) {
+//             if(logger){
+//                 const myLogger = {
+//                     logger: m => console.log(m)
+//                 }
+//                 recognizeResult = await tesseract.recognize(fileName, lang, myLogger)
+//             }  else {
+//                 recognizeResult = await tesseract.recognize(fileName, lang);
+
+//             }
+//             if (recognizeResult) {
+//                 return recognizeResult.data.text
+//             }
+//         }
+//     } catch (error)   { 
+//         return error.message
+//     }   
+
+// }
+
+// getImageText("imagetext.jpg", "eng", true).then(content =>{
+//     console.log(content)
+// }).catch(errMsg => {
+//     console.log(errMsg)
+// })
+    
